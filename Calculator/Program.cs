d87971da5ae276d863c89e4b4570b989f7005d17
pakomo1 <83059895+pakomo1@users.noreply.github.com>
2022-04-09 14:14:52 +0300

    create input-outpit functionality

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,7 +6,44 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string command = Console.ReadLine().ToLower();
+
+            while (command != "End")
+            {
+                if (command == "add")
+                {
+
+                }
+                else if (command == "subtract")
+                {
+
+                }
+                else if (command == "divide")
+                {
+
+                }
+                else if (command == "power")
+                {
+
+                }
+                command = Console.ReadLine().ToLower();
+            }
+            static double Add(double num1, double num2)
+            {
+
+            }
+            static double Subtract(double num1, double num2)
+            {
+
+            }
+            static double Divide(double num1, double num2)
+            {
+
+            }
+            static double Power(double num1, double num2)
+            {
+
+            }
         }
     }
 }
