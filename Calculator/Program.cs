43a25b7be51ed2f03fa5048c1a4eca740332c8bd
velikow <83334334+velikow@users.noreply.github.com>
2022-04-09 12:19:44 +0300

    add input/output functionality

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,7 +6,35 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string command =  Console.ReadLine();
+            int result = int.MinValue;
+            while (command != "End")
+            {
+                switch (command)
+                {
+                    case "Multiply":
+                        int number1 = int.Parse(Console.ReadLine());
+                        int number2 = int.Parse(Console.ReadLine());
+
+                        result = Multiply(number1, number2);
+                        break;
+                    case "End":
+                        break;
+                        default:
+                        Console.WriteLine("Invalid command!");
+                        break;
+                }
+                if (result != int.MinValue)
+                {
+                    Console.WriteLine($"Result: {result} ");
+                }
+                command = Console.ReadLine();
+            }
+        }
+
+        private static int Multiply(int number1, int number2)
+        {
+            throw new NotImplementedException();
         }
     }
 }
